25a892cfd522f7eb666b2a0bf8326f0064beca41
thechungster <timmmmy400@gmail.com>
2014-11-20 17:58:28 -0600

    test database

---
+++ assets/php/database.php
@@ -0,0 +1,8 @@
+<?php
+
+	$servername = "mllu1gheq2.database.secure.windows.net,1433";																// ip of the database
+	$username = "microsoft";																		// user name of the databse
+	$password = "Password1!";																			//password to access the databse
+	$dbname = "userinfo";		
+
+?>
