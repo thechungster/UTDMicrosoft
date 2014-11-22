64f149cd882fed8729ac3a454ab707deae12bf78
thechungster <timmmmy400@gmail.com>
2014-11-22 11:40:18 -0600

    try localhost db

--- assets/php/database.php
+++ assets/php/database.php
@@ -1,8 +1,8 @@
 <?php
 
-	$servername = "mllu1gheq2.database.secure.windows.net,1433";																// ip of the database
+	$servername = "localhost";																// ip of the database
 	$username = "microsoft";																		// user name of the databse
-	$password = "Password1!";																			//password to access the databse
+	$password = "Password1";																			//password to access the databse
 	$dbname = "userinfo";		
 
 	$conn = mysql_connect($servername, $username, $password) or die(mysql_error());			//connect to the database
